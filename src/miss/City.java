4be5c258047e2727530b9f56017f7e403eb0f3a8
drewniak <aleksandra.drewniak1a@gmail.com>
2017-04-18 19:19:35 +0200

    add City class as singleton

---
+++ src/miss/City.java
@@ -0,0 +1,23 @@
+package miss;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class City {
+
+	public List<Crossing> crossings = new ArrayList<Crossing>();
+	private static final City instance = new City();
+	private City(){
+		
+	}
+	public static City getInstance(){
+		return instance;
+	}
+	// tak sobie mysle, że lepszym pomysłem było trzymanie tego jako grafu,
+	// bo jeżeli będziemy chcieli szukać jakiejś drogi z punktu A do punktu B 
+	// to znalezienie drogi na grafie będzie proste, ale myślę, że to można spokojnie
+	// zmienić później, a teraz wystarczy to po prostu jako lista skrzyżowań. 
+	
+
+	
+}
