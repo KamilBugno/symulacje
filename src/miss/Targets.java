e3f866b23eaf92f0ade35e0867605a574b923b0c
drewniak <aleksandra.drewniak1a@gmail.com>
2017-06-07 16:15:00 +0200

    change id

--- src/miss/Targets.java
+++ src/miss/Targets.java
@@ -23,56 +23,56 @@ public void createTargets(){
  ids = new ArrayList<>(Arrays.asList(0)); //	0 -> 0
  targets.put(0, createRoadList(ids));
  ids = new ArrayList<>(Arrays.asList(12, 10)); //	1 -> 12, 10
- targets.put(0, createRoadList(ids));
+ targets.put(1, createRoadList(ids));
  ids = new ArrayList<>(Arrays.asList(23,28)); //	2 -> 23, 28
- targets.put(0, createRoadList(ids));
+ targets.put(2, createRoadList(ids));
  ids = new ArrayList<>(Arrays.asList(5)); //	3 -> 5
- targets.put(0, createRoadList(ids));
+ targets.put(3, createRoadList(ids));
  ids = new ArrayList<>(Arrays.asList(16,6,10)); //	4 -> 16, 6, 10
- targets.put(0, createRoadList(ids));
+ targets.put(4, createRoadList(ids));
  ids = new ArrayList<>(Arrays.asList(5)); //	5 -> 5
- targets.put(0, createRoadList(ids));
+ targets.put(5, createRoadList(ids));
  ids = new ArrayList<>(Arrays.asList(13,23,25)); //	6 -> 13, 23, 25
- targets.put(0, createRoadList(ids));
+ targets.put(6, createRoadList(ids));
  ids = new ArrayList<>(Arrays.asList(9)); //	7 -> 9
- targets.put(0, createRoadList(ids));
+ targets.put(7, createRoadList(ids));
  ids = new ArrayList<>(Arrays.asList(17, 2, 23, 25)); //	8 -> 17, 2, 23, 25
- targets.put(0, createRoadList(ids));
+ targets.put(8, createRoadList(ids));
  ids = new ArrayList<>(Arrays.asList(9)); //	9 -> 9
- targets.put(0, createRoadList(ids));
+ targets.put(9, createRoadList(ids));
  ids = new ArrayList<>(Arrays.asList(10)); //	10 -> 10
- targets.put(0, createRoadList(ids));
+ targets.put(10, createRoadList(ids));
 
  ids = new ArrayList<>(Arrays.asList(13, 23, 21)); // 11 -> 13, 23, 21
- targets.put(0, createRoadList(ids));
+ targets.put(11, createRoadList(ids));
  ids = new ArrayList<>(Arrays.asList(7, 9)); // 12 -> 7, 9
- targets.put(0, createRoadList(ids));
+ targets.put(12, createRoadList(ids));
  ids = new ArrayList<>(Arrays.asList(23, 21)); // 13 -> 23, 21
- targets.put(0, createRoadList(ids));
+ targets.put(13, createRoadList(ids));
  ids = new ArrayList<>(Arrays.asList(14)); // 14 -> 14
- targets.put(0, createRoadList(ids));
+ targets.put(14, createRoadList(ids));
  ids = new ArrayList<>(Arrays.asList(17, 2, 0)); // 15 -> 17, 2, 0
- targets.put(0, createRoadList(ids));
+ targets.put(15, createRoadList(ids));
  ids = new ArrayList<>(Arrays.asList(14)); // 16 -> 14
- targets.put(0, createRoadList(ids));
+ targets.put(16, createRoadList(ids));
  ids = new ArrayList<>(Arrays.asList(2, 0)); // 17 -> 2, 0
- targets.put(0, createRoadList(ids));
+ targets.put(17, createRoadList(ids));
  ids = new ArrayList<>(Arrays.asList(18)); // 18 -> 18
- targets.put(0, createRoadList(ids));
+ targets.put(18, createRoadList(ids));
  ids = new ArrayList<>(Arrays.asList(22, 12, 10)); // 19 -> 22, 12, 10
- targets.put(0, createRoadList(ids));
+ targets.put(19, createRoadList(ids));
  ids = new ArrayList<>(Arrays.asList(25)); // 20 -> 25
- targets.put(0, createRoadList(ids));
+ targets.put(20, createRoadList(ids));
  ids = new ArrayList<>(Arrays.asList(21)); // 21 -> 21
- targets.put(0, createRoadList(ids));
+ targets.put(21, createRoadList(ids));
  ids = new ArrayList<>(Arrays.asList(3, 5)); // 22 -> 3, 5
- targets.put(0, createRoadList(ids));
+ targets.put(22, createRoadList(ids));
  ids = new ArrayList<>(Arrays.asList(18)); // 23 -> 18
- targets.put(0, createRoadList(ids));
+ targets.put(23, createRoadList(ids));
  ids = new ArrayList<>(Arrays.asList(22, 3, 16, 14)); // 24 -> 22, 3, 16, 14
- targets.put(0, createRoadList(ids));
+ targets.put(24, createRoadList(ids));
  ids = new ArrayList<>(Arrays.asList(25)); // 25 -> 25
- targets.put(0, createRoadList(ids));
+ targets.put(25, createRoadList(ids));
 	
 }
 
